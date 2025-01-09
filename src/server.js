6deec5dcedd54553976f6af447bb2e9f55f5c49c
Gelzieny R. Martins <gelzieny@gmail.com>
2025-01-09 14:18:41 -0300

    Filtrando lista do banco de dados

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import http from 'node:http'
 
 import { json } from './middlewares/json.js'
 import { routes } from './routes.js'
+import { extractQueryParams } from './utils/extract-query-params.js'
 
 // Query Parameters: URL Stateful => Filtros, paginação, não-obrigatórios
 // Route Parameters: Identificação de recurso
@@ -16,16 +17,24 @@ import { routes } from './routes.js'
 
 // Edição e remoção
 
+
 const server = http.createServer(async (req, res) => {
   const { method, url } = req
 
   await json(req, res)
 
   const route = routes.find(route => {
-    return route.method === method && route.path === url
+    return route.method === method && route.path.test(url)
   })
 
   if (route) {
+    const routeParams = req.url.match(route.path)
+
+    const { query, ...params } = routeParams.groups
+
+    req.params = params
+    req.query = query ? extractQueryParams(query) : {}
+
     return route.handler(req, res)
   }
 
