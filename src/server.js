c4ae226e6c9f1a88a33f2540b675446afd9c3b7d
Gelzieny R. Martins <gelzieny.martins@goias.gov.br>
2025-04-05 13:01:55 -0300

    Criando rotas (Metodos HTTP)

--- src/server.js
+++ src/server.js
@@ -1,8 +1,39 @@
-import http from 'http'
+import http from 'node:http'
+
+// - Criar usuários
+// - Listagem usuários
+// - Edição de usuários
+// - Remoção de usuários
+
+// - HTTP
+//   - Método HTTP
+//   - URL
+
+// GET, POST, PUT, PATCH, DELETE
+
+// GET => Buscar um recurso do back-end
+// POST => Criar um recurso no back-end
+// PUT => Atualizar um recurso no back-end
+// PATCH => Atualizar uma informação específica de um recurso no back-end
+// DELETE => Deletar um recurso do back-end
+
+// GET /users => Buscando usuários no banc-end
+// POST /users => Criar um usuário no back-end
 
 const server = http.createServer((req, res) => {
-  return res.end('Ola mundo')
+  const { method, url } = req
+
+  if (method === 'GET' && url === '/users') {
+    return res.end('Listagem de usuários')
+  }
+
+  if (method === 'POST' && url === '/users') {
+    return res.end('Criação de usuário')
+  }
+
+  return res.end('Hello World')
 })
+
 
 server.listen(3333, () => {
   console.log('🚀 Servidor rodando na porta 3333');
